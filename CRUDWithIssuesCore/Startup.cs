6b6009757e7d651188da03c88153ae84b8dae9f9
Mary Saray <msaray.ms@outlook.com>
2021-08-29 06:11:07 -0700

    Added suggested package for IHostingEnviornment deprecated warning

--- CRUDWithIssuesCore/Startup.cs
+++ CRUDWithIssuesCore/Startup.cs
@@ -43,6 +43,7 @@ namespace CRUDWithIssuesCore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        [Obsolete]
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
